2891d303eb85f6d2bb15bc6a442c4b09c118ca86
alessandrocruz5 <alessandrorafaelcruz@gmail.com>
2024-03-21 22:28:08 +0800

    auth pt 2

--- src/app/components/dashboard/card/card.component.ts
+++ src/app/components/dashboard/card/card.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Emitters } from 'src/app/emitters/emitters';
 import { ChartService } from 'src/app/services/chart.service';
 
 @Component({
@@ -9,10 +10,26 @@ import { ChartService } from 'src/app/services/chart.service';
 })
 export class CardComponent implements OnInit {
   cards: any[] = [];
+  authenticated = false;
+  message: any;
 
   constructor(private chartService: ChartService, private http: HttpClient) {}
 
   ngOnInit() {
+    this.http
+      .get('http://localhost:1217/api/user', {
+        withCredentials: true,
+      })
+      .subscribe(
+        (res: any) => {
+          Emitters.authEmitter.emit((this.authenticated = true));
+        },
+        (err) => {
+          this.message = 'You are not logged in.';
+          Emitters.authEmitter.emit((this.authenticated = false));
+        }
+      );
+
     this.fetchCards();
   }
 
